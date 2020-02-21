e22405311a45d9c0abb2cbd5dec4e13af48293f2
vedeeva <victoria.edeeva@gmail.com>
2020-02-20 19:05:22 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Victoria Edeeva on 2/20/20.
+//  Copyright © 2020 Victoria Edeeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
